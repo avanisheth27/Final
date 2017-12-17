ba590e7bad2f4a26668f8d56059be2eab7907fed
Avani V Sheth <avs44@afsconnect2.njit.edu>
2017-12-17 15:04:10 -0500

    added

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,6 +1,46 @@
 <!doctype html>
 
 <html lang="en">
+<style>
+form {
+    border: 0px solid #f1f1f1;
+}
+
+body {
+   
+   background-color: #42f4aa;
+}
+
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 08px 0;
+    display: inline-block;
+    background:  ;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+button  {
+    background-color: #000098;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 30%;
+    
+}
+
+button:hover {
+    opacity: 0.8;
+}
+.container {
+    margin: 0 175px;
+    padding: 16px;
+}
+
+</style>
 <head>
     <meta charset="utf-8">
 
