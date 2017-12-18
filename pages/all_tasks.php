94b199f78bc11a45809c8983ec5da54a7f6170b3
avanisheth27 <31868200+avanisheth27@users.noreply.github.com>
2017-12-18 16:43:45 -0500

    added

--- pages/all_tasks.php
+++ pages/all_tasks.php
@@ -1,21 +1,8 @@
 <!doctype html>
 
 <html lang="en">
-
 <body bgcolor="#f4cb42">
 <style>
-input {
-    width: 20%;
-    background-color: #000000;
-    font-size: 12px;
-    color: white;
-    padding: 12px 20px;
-    margin: 8px auto;
-    border: none;
-    cursor: pointer;
-    width: 30%;
-    
-}
 button  {
     width: 20%;
     background-color: #000000;
@@ -28,11 +15,6 @@ button  {
     width: 30%;
     
 }
-
-button:hover {
-    opacity: 0.8;
-}
-
 </style>
 <head>
     <meta charset="utf-8">
@@ -43,11 +25,16 @@ button:hover {
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
 
-   
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
 </head>
 
 <body>
 <h1 align="left"><u><b>Todo Task Manangement</b></u></h1>
+
+
+
 <?php
 $userID = $_SESSION["userID"];
 $user_data = accounts::findUserbyId($userID); ?>
@@ -56,27 +43,14 @@ $user_data = accounts::findUserbyId($userID); ?>
 
 
 
-
-<div>
-
-
-<button onclick ="window.location.href='index.php?page=accounts&action=all'">My Profile</button><br>
+<button onclick = "window.location.href='index.php?page=tasks&action=all'"> Todos </button><br>
+<button onclick = "window.location.href='index.php?page=accounts&action=logout'"> Logout </button>
 
 
-<button onclick ="window.location.href='index.php?page=tasks&action=create'">Add Todos</button><br>
-
-
-<button onclick ="window.location.href='index.php?page=accounts&action=logout'">Logout</button><br>
-
-</div>
 
 <?php
 //this is how you print something
-if(!empty($data)) {
-	print utility\htmlTable::genarateTableFromMultiArray($data);
-} else {
-	echo 'Data not found';
-}
+print utility\htmlTable::genarateTableFromMultiArray($data);
 ?>
 
 <script src="js/scripts.js"></script>
