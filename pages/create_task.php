bb70b66f2281be60ad24f2c7bddca23656f88726
avanisheth27 <31868200+avanisheth27@users.noreply.github.com>
2017-12-18 08:48:49 -0500

    Update create_task.php

--- pages/create_task.php
+++ pages/create_task.php
@@ -1,6 +1,37 @@
 <!doctype html>
 
 <html lang="en">
+<body bgcolor="#f4cb42">
+<style>
+
+form { width: 200px; }
+
+label { float: left; width: 100px; }
+
+input[type=text] { float: left; 
+width: 300px;
+height:70px 
+}
+
+.clear { clear: both; height: 0; line-height: 0; }
+
+.floatright { float: right; }
+button  {
+    width: 40%;
+    background-color: #000000;
+    font-size: 14px;
+    color: white;
+    padding: 22px 75px;
+    margin: 8px auto;
+    border: none;
+    cursor: pointer;
+    
+    
+}
+
+
+</style>
+
 <head>
     <meta charset="utf-8">
 
@@ -16,20 +47,15 @@
 </head>
 
 <body>
-<h1>
-<?php
-//this how to print some data;
-$config = Manage::config();
-echo $config['site_name'];
-?></h1>
-<h1>Add Todos</h1>
 
+    <h1 align="left"><u><b>Todo Task Manangement</b></u></h1>
 <?php
 $userID = $_SESSION["userID"];
-$user_data = accounts::findUserbyId($userID);
-?>
+$user_data = accounts::findUserbyId($userID); ?>
 
-<h1>Hi <?=$user_data->fname?></h1>
+<h1>Username : <?=$user_data->fname?></h1>
+
+
 								
 <ul>
 <li><a href="index.php?page=accounts&action=all">My Account</a>
@@ -42,10 +68,17 @@ $user_data = accounts::findUserbyId($userID);
 </ul>
 
 <form action="index.php?page=tasks&action=save" method="post">
-	Title: <input type="text" name="title" id="title" required><br>
-	Body: <textarea name="body" id="body" required></textarea><br>
-	Is Done: <input type="checkbox" name="isdone" id="isdone" value="1"><br>
-    <input type="submit" value="Submit form">
+
+<label for="title">	<b>Title</b>: <input type="text" name="title" id="title" value=""><br><br><br><br>
+
+
+<b>Body</b>: <textarea rows="10" cols="50"; name="body" id="body" required></textarea>
+<br><br>
+ 
+	<label for="isdone"><b>Is Done</b>: <input type="checkbox" name="isdone" id="isdone" value="1"><br><br>
+ 
+     <button type="submit">Submit</button><br>
+     
 </form>
 <a href="index.php?page=tasks&action=all">Back</a>
 <script src="js/scripts.js"></script>
