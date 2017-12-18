12ac33a93d42fad9098ede0df6e55a823195535f
avanisheth27 <31868200+avanisheth27@users.noreply.github.com>
2017-12-18 10:40:18 -0500

    Update homepage.php

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,77 +1,40 @@
 <!doctype html>
 
 <html lang="en">
+<body bgcolor="#f4cb42">
 
-<style>
-form {
-    border: 0px solid #f1f1f1;
-}
-
-body {
-   
-   background-color:#f4cb42;
-}
-
-input[type=text], input[type=password] {
-    width: 20%;
-    padding: 12px 20px;
-    margin: 08px auto;
-    display: inline-block;
-    border: 1px solid #ccc;
-    box-sizing: border-box;
-}
-
-button  {
-    width: 20%;
-    background-color: #000000;
-    font-size: 12px;
-    color: white;
-    padding: 12px 20px;
-    margin: 8px auto;
-    border: none;
-    cursor: pointer;
-    width: 30%;
-    
-}
-
-button:hover {
-    opacity: 0.8;
-}
-.container {
-    margin: 0 175px;
-    padding: 16px;
-}
-</style>
 <head>
+<link rel="stylesheet" type ="text/css" href="pages/stylesheet.css">
     <meta charset="utf-8">
 
     <title>Todo Task Management</title>
-    <h1 align="left"><u><b>Todo Task Manangement</b></u></h1>
+    <br><br><br>
+    <h1 align="center"><u><b>Todo Task Manangement</b></u></h1>
 
     <meta name="description" content="Todo Task Management">
     <meta name="author" content="SitePoint">
 
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
+  
 
     
 </head>
 
+
 <body>
 <div class = "overall">
 
-
-
-
-<form action="index.php?page=accounts&action=login" method="POST">
-<div class="form-input">
+<form action="index.php?page=accounts&action=login" method="POST" align="center">
+<div class="input">
         <div class="center">
         <label><b>Username</b></label>
         <input type="text" placeholder="Enter Username" name="email" required><br>
-        </div>
+        <br><br>
 
         <label><b>Password</b></label>
         <input type="password" placeholder="Enter Password" name="password" required><br>
+        </div>
 <br>
+<div class = "button">
         
         <button type="submit">Login</button>
     </div>
