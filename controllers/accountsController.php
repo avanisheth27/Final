30652046b4eb4cac94eeb2603fd25ce2a6545f37
avanisheth27 <31868200+avanisheth27@users.noreply.github.com>
2017-12-17 20:15:06 -0500

    added

--- controllers/accountsController.php
+++ controllers/accountsController.php
@@ -33,23 +33,17 @@ class accountsController extends http\controller
         self::getTemplate('all_accounts', $records);
 
     }
-    //to call the show function the url is called with a post to: index.php?page=task&action=create
-    //this is a function to create new tasks
-
-    //you should check the notes on the project posted in moodle for how to use active record here
-
-    //this is to register an account i.e. insert a new account
+    
     public static function register()
     {
-		//By IPE TEAM
+		
 		session_start();
 		$userID = @$_SESSION["userID"];
 		if($userID) {
 			header("Location: index.php?page=accounts&action=all");
 		}
 		
-        //https://www.sitepoint.com/why-you-should-use-bcrypt-to-hash-stored-passwords/
-        //USE THE ABOVE TO SEE HOW TO USE Bcrypt
+        
         self::getTemplate('register');
     }
 
@@ -68,22 +62,14 @@ class accountsController extends http\controller
             $user->phone = $_POST['phone'];
             $user->birthday = $_POST['birthday'];
             $user->gender = $_POST['gender'];
-            //$user->password = $_POST['password'];
-            //this creates the password
-            //this is a mistake you can fix...
-            //Turn the set password function into a static method on a utility class.
+            
             $user->password = $user->setPassword($_POST['password']);
             $user->save();
 
-            //you may want to send the person to a
-            // login page or create a session and log them in
-            // and then send them to the task list page and a link to create tasks
             header("Location: index.php?page=accounts&action=all");
 
         } else {
-            //You can make a template for errors called error.php
-            // and load the template here with the error you want to show.
-           // echo 'already registered';
+            
             $error = 'already registered';
             self::getTemplate('error', $error);
 
@@ -123,13 +109,7 @@ class accountsController extends http\controller
     //this is to login, here is where you find the account and allow login or deny.
     public static function login()
     {
-        //you will need to fix this so we can find users username.  YOu should add this method findUser to the accounts collection
-        //when you add the method you need to look at my find one, you need to return the user object.
-        //then you need to check the password and create the session if the password matches.
-        //you might want to add something that handles if the password is invalid, you could add a page template and direct to that
-        //after you login you can use the header function to forward the user to a page that displays their tasks.
-        //        $record = accounts::findUser($_POST['email']);
-
+        
         $user = accounts::findUserbyEmail($_REQUEST['email']);
         if($user == FALSE) {
             echo 'user not found';
@@ -147,7 +127,7 @@ class accountsController extends http\controller
         }
     }
 	
-	//By IPE TEAM
+
     public static function logout()
     {
         session_start();
